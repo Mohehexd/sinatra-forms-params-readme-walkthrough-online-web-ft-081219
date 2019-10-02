9d83d2ec46d24c6ff245032ef2378df0aa9e4b0b
Guillermo Martinez <dumblemoe@gmail.com>
2019-10-02 15:56:05 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,10 +5,4 @@ class App < Sinatra::Base
   get '/food_form' do
     erb :food_form
   end
-
-  <form>
-    <p>Your Name:<input type="text"></p>
-    <p>Your Favorite Food: <input type="text"></p>
-    <input type="submit">
-  </form>
 end
