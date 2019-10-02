72365f5908eea8968dc4b3fb1187a45b71dace9d
Guillermo Martinez <dumblemoe@gmail.com>
2019-10-02 16:09:03 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,11 +2,12 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-    get '/food_form' do
+  get '/food_form' do
       erb :food_form
-    end
-
-    post '/food' do
-    params.to_s
   end
+
+  post '/food' do
+    "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
+  end
+  
 end
